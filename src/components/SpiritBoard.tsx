85a30d25947e6b075ac78995d66b1ced37473c8f
11292-stella <stella.marucelli@gmail.com>
2025-10-26 14:46:51 +0100

    Messaggio del commit

--- src/components/SpiritBoard.tsx
+++ src/components/SpiritBoard.tsx
@@ -1,26 +1,65 @@
-import { Container, Row, Col, Image } from "react-bootstrap"
-
+import { Container, Row, Col } from "react-bootstrap"
+import { useState } from "react"
 import "../styles/spiritboard.css"
+
 const SpiritBoard = function () {
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    setClicked(true)
+    setTimeout(() => setClicked(false), 600)
+
+    const sound = document.getElementById("horror-sound") as HTMLAudioElement
+    if (sound) sound.play()
+
+    triggerBats()
+  }
+
+  const triggerBats = () => {
+    const bats = document.querySelectorAll(".bat")
+    bats.forEach((bat) => {
+      bat.classList.remove("fly")
+      void (bat as HTMLElement).offsetWidth // forza il reflow
+      bat.classList.add("fly")
+    })
+  }
+
   return (
     <>
       <section id="spirit-board" className="py-5">
+        {/* Audio creepy */}
+        <audio id="horror-sound" src="/sounds/creepy.mp3" preload="auto" />
+
         <div className="page-background d-flex flex-column min-vh-100">
           <Container className="flex-grow-1 py-5">
+            {/* Pipistrelli */}
+            <div className="bats">
+              <img src="/simpoli/bat.png" className="bat bat1" />
+              <img src="/simpoli/bat1.png" className="bat bat2" />
+            </div>
+
             <Row className="justify-content-center">
               <Col
-                md={8}
-                lg={6}
+                xs={12}
+                md={10}
+                lg={10}
                 className="d-flex flex-column align-items-center text-light"
               >
-                <div className="spiritboard-wrapper">
-                  <div className="spiritboard-container">
+                <div className="spiritboard">
+                  <img
+                    src="/tavola.png"
+                    alt="Spirit Board"
+                    className="spiritboard-img"
+                  />
+
+                  {/* Simbolo 1 */}
+                  <div className="img1">
                     <img
-                      src="/tavola.png"
-                      alt="Spirit Board"
-                      className="spiritboard-img"
+                      src="/simpoli/1.png"
+                      alt="Simbolo 1"
+                      className={`symbol-img1 ${clicked ? "clicked" : ""}`}
+                      onClick={handleClick}
                     />
-                    <div className="overlay">{/* simboli cliccabili */}</div>
                   </div>
                 </div>
               </Col>
@@ -31,4 +70,5 @@ const SpiritBoard = function () {
     </>
   )
 }
+
 export default SpiritBoard
